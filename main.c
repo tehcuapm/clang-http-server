a4e2b2aab0d7cc62dfd0a36c26027ec2d9430759
tehcuapm <tehcuapm@gmail.com>
2025-03-02 00:49:02 +0100

    feat: first step

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(void) {
+
+  struct sockaddr_in server_addr = {
+    .sin_family = AF_INET,
+    .sin_port   = htons(8080),
+    .sin_addr   = { INADDR_ANY },
+    .sin_zero   = { 0 }
+  };
+
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  };
+
+  // On attend une requête avant de renvoyer ECONNREFUSED
+  if (listen(sockfd, 1) < 0) {
+    perror("listen failed");
+    exit(EXIT_FAILURE);
+  };
+
+  int client_fd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+  if (client_fd == -1) {
+    perror("Erreur lors de l'acceptation (accept)");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Connexion acceptée de %s:%d\n",
+       inet_ntoa(client_addr.sin_addr),
+       ntohs(client_addr.sin_port));
+
+  close(client_fd);
+  close(sockfd);
+
+  return 0;
+}
